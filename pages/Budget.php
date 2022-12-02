e25173fd6f7fee38b54a1af4cc747c847a3e883d
ShikamaruBH <huynhphuquy712001asus4.4.2@gmail.com>
2022-12-02 16:45:50 +0700

    [11] More month options

--- pages/Budget.php
+++ pages/Budget.php
@@ -30,6 +30,17 @@
                                         <label for="month">Month</label>
                                         <select name="month" class="form-control">
                                             <option>January</option>
+                                            <option>February</option>
+                                            <option>March</option>
+                                            <option>April</option>
+                                            <option>May</option>
+                                            <option>June</option>
+                                            <option>July</option>
+                                            <option>August</option>
+                                            <option>September</option>
+                                            <option>October</option>
+                                            <option>November</option>
+                                            <option>December</option>
                                         </select>
                                     </div>
                                     <div class="form-group pull-left">
